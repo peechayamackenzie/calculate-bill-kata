25f2963c3cec09f9e5626a80135114077e5225bf
Peechaya Mackenzie <tay_eee@hotmail.com>
2020-12-15 19:29:52 +0000

    Added solution

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+const calculateBill = (price, vat, tip) => {
+    if (typeof(price, vat, tip) === "number") {
+        const amount = price + (price * vat / 100) + tip;
+        return "£" + amount;
+    }else{
+        return "error";
+    }
+}
 module.exports = calculateBill;
